9ffe7340f01546daf26dacd28bc3046122fa78f2
Ruslan Pidhainyi <Ruslan.Pidhainyi@microsoft.wsei.edu.pl>
2023-07-04 17:27:05 +0200

    Add model userStudent

--- AppCore/Interfaces/Identity/Identity.cs
+++ AppCore/Interfaces/Identity/Identity.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace AppCore.Interfaces.Identity
 {
-    internal class Identity
+    public interface IIdentity<K> : IComparable<K> where K : IComparable<K>
     {
+        public K Id
+        {
+            get;
+            set;
+        }
+
+        int IComparable<K>.CompareTo(K? other)
+        {
+            return CompareTo(other);
+        }
     }
 }
